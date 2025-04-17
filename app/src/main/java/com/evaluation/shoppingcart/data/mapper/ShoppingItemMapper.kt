f99c47d14cbf3811f48815b99d344a89bf39fb3b
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-17 13:34:02 +0530

    Add mapper to map ShoppingItem domain model to ShoppingCartEntity database model.

--- app/src/main/java/com/evaluation/shoppingcart/data/mapper/ShoppingItemMapper.kt
+++ app/src/main/java/com/evaluation/shoppingcart/data/mapper/ShoppingItemMapper.kt
@@ -1,5 +1,6 @@
 package com.evaluation.shoppingcart.data.mapper
 
+import com.evaluation.shoppingcart.data.local.entity.ShoppingCartEntity
 import com.evaluation.shoppingcart.data.remote.dto.shoppingItems.ShoppingItemDto
 import com.evaluation.shoppingcart.domain.model.shoppingItems.ShoppingItem
 
@@ -9,4 +10,13 @@ fun ShoppingItemDto.toShoppingItem(): ShoppingItem =
         itemName = itemName,
         sellingPrice = sellingPrice,
         taxPercentage = taxPercentage,
+    )
+
+fun ShoppingItem.toShoppingCartItem(quantity: Int = 1): ShoppingCartEntity =
+    ShoppingCartEntity(
+        itemID = itemID!!,
+        itemName = itemName,
+        sellingPrice = sellingPrice,
+        taxPercentage = taxPercentage,
+        quantity = quantity,
     )
