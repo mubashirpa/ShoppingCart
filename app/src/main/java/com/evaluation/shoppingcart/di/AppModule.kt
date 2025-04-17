c896e4de511fce7c2696573279e43e6d893c9c97
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-17 13:55:04 +0530

    Add CartViewModel, GetShoppingCartItemsUseCase, and ShoppingCartRepository binding.

--- app/src/main/java/com/evaluation/shoppingcart/di/AppModule.kt
+++ app/src/main/java/com/evaluation/shoppingcart/di/AppModule.kt
@@ -7,7 +7,9 @@ import com.evaluation.shoppingcart.data.repository.ShoppingItemsRepositoryImpl
 import com.evaluation.shoppingcart.domain.repository.ShoppingCartRepository
 import com.evaluation.shoppingcart.domain.repository.ShoppingItemsRepository
 import com.evaluation.shoppingcart.domain.usecase.AddToShoppingCartUseCase
+import com.evaluation.shoppingcart.domain.usecase.GetShoppingCartItemsUseCase
 import com.evaluation.shoppingcart.domain.usecase.GetShoppingItemsUseCase
+import com.evaluation.shoppingcart.presentation.cart.CartViewModel
 import com.evaluation.shoppingcart.presentation.home.HomeViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -39,7 +41,9 @@ val appModule =
         singleOf(::ShoppingCartRepositoryImpl) { bind<ShoppingCartRepository>() }
         single { GetShoppingItemsUseCase(get()) }
         single { AddToShoppingCartUseCase(get()) }
+        single { GetShoppingCartItemsUseCase(get()) }
         viewModelOf(::HomeViewModel)
+        viewModelOf(::CartViewModel)
         single {
             Room
                 .databaseBuilder(
