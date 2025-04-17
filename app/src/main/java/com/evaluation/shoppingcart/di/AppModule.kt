375f2b6682dd8ee249969181a5bb4f31c1bc0cf0
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-17 12:10:42 +0530

    Add Hilt dependencies and HomeViewModel.

--- app/src/main/java/com/evaluation/shoppingcart/di/AppModule.kt
+++ app/src/main/java/com/evaluation/shoppingcart/di/AppModule.kt
@@ -3,12 +3,14 @@ package com.evaluation.shoppingcart.di
 import com.evaluation.shoppingcart.data.repository.ShoppingItemsRepositoryImpl
 import com.evaluation.shoppingcart.domain.repository.ShoppingItemsRepository
 import com.evaluation.shoppingcart.domain.usecase.GetShoppingItemsUseCase
+import com.evaluation.shoppingcart.presentation.home.HomeViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule =
@@ -29,4 +31,5 @@ val appModule =
         }
         singleOf(::ShoppingItemsRepositoryImpl) { bind<ShoppingItemsRepository>() }
         single { GetShoppingItemsUseCase(get()) }
+        viewModelOf(::HomeViewModel)
     }
