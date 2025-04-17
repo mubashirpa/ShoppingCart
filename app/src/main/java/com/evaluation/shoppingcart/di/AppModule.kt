af5801164070c045c455eb174d102a2a24af74b2
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-17 11:30:20 +0530

    Add Koin module for dependency injection, including HttpClient setup and repository/use case bindings.

---
+++ app/src/main/java/com/evaluation/shoppingcart/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.evaluation.shoppingcart.di
+
+import com.evaluation.shoppingcart.data.repository.ShoppingItemsRepositoryImpl
+import com.evaluation.shoppingcart.domain.repository.ShoppingItemsRepository
+import com.evaluation.shoppingcart.domain.usecase.GetShoppingItemsUseCase
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+
+val appModule =
+    module {
+        single {
+            HttpClient {
+                expectSuccess = true
+                install(ContentNegotiation) {
+                    json(
+                        Json {
+                            ignoreUnknownKeys = true
+                            isLenient = true
+                            useAlternativeNames = false
+                        },
+                    )
+                }
+            }
+        }
+        singleOf(::ShoppingItemsRepositoryImpl) { bind<ShoppingItemsRepository>() }
+        single { GetShoppingItemsUseCase(get()) }
+    }
